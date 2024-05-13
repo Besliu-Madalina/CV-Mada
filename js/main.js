d8b6190cd4ec2f4a6e54a792302b240025c8ffa7
Besliu-Madalina <104641549+Besliu-Madalina@users.noreply.github.com>
2024-05-13 16:27:05 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+new SimpleLightbox({ elements: '.gallery a' });
+new SimpleLightbox({ elements: '.gallery2 a' });
+
+// function show(){
+//     document.getElementById("side-menu").classList.toggle("show");
+// }
